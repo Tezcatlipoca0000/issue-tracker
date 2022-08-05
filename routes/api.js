e00dc1e6cc492305f7cc0ad7a5630d354b32f4ce
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-04 22:07:49 -0500

    made progress

--- routes/api.js
+++ routes/api.js
@@ -17,37 +17,45 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
-      // create model here... project = model
+      let project = req.params.project,
+          Issue = mongoose.model(project, issue);
+      Issue.find({}, (err, docs) => {
+        err ? console.log('finding error', err) : res.json(docs);
+      });
     })
     
     .post(function (req, res){
-      let project = req.params.project;
-      console.log('oiiii', project);
-      const Issue = mongoose.model(project, issue),
-            newIssue = new Issue({
-              issue_title: 'Testing1',
-              issue_text: 'tst1',
-              created_on: new Date(),
-              updated_on: new Date(),
-              created_by: 'tez',
-              assigned_to: 'tez',
-              open: true,
-              status_text: 'testing'
-            });
+      let project = req.params.project,
+          Issue = mongoose.model(project, issue),
+          newIssue = new Issue({
+            issue_title: req.body.issue_title,
+            issue_text: req.body.issue_text,
+            created_on: new Date(),
+            updated_on: new Date(),
+            created_by: req.body.created_by,
+            assigned_to: req.body.assigned_to,
+            open: true,
+            status_text: req.body.status_text
+          });
       newIssue.save((err, data) => {
-        err ? console.log('error saving', err) : console.log('issue saved', data);
+        err ? console.log('error saving', err) : res.json(data);
       });
     })
     
     .put(function (req, res){
-      let project = req.params.project;
-      
+      let project = req.params.project,
+          Issue = mongoose.model(project, issue);
+      Issue.findByIdAndUpdate(req.body._id, {open: false}, {returnDocument: 'after'}, (err, doc) => {
+        err ? console.log('error updating', err) : res.json(doc);
+      });
     })
     
     .delete(function (req, res){
-      let project = req.params.project;
-      
+      let project = req.params.project,
+          Issue = mongoose.model(project, issue);
+      Issue.deleteOne({_id: req.body._id}, (err, doc) => {
+        err ? console.log('error deleting', err) : res.json(doc);
+      });
     });
     
 };
