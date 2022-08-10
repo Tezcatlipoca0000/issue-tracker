53d8b3ed9f13b93fe0c5be3c6ba11a52232b03cd
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2022-08-09 21:07:04 -0500

    removed all the console logs

--- routes/api.js
+++ routes/api.js
@@ -20,9 +20,8 @@ module.exports = function (app) {
       let project = req.params.project,
           Issue = mongoose.model(project, issue),
           queries = req.query;
-      console.log('getting ', project, '& filtered ', queries);
       Issue.find(queries, (err, docs) => {
-        (err || docs === null) ? (console.log('error getting docs ', err, docs), res.json({error: 'error'})) : (console.log('the found docs & typeof ', err, docs, typeof docs), res.json(docs))
+        (err || docs === null) ? res.json({error: 'error'}) : res.json(docs)
       });
     })
     
@@ -39,34 +38,29 @@ module.exports = function (app) {
             created_on: new Date(),
             updated_on: new Date(),
           });
-      console.log('posting on ', project, 'the issue ', newIssue);
       newIssue.save((err, data) => {
-        err ? (console.log('error posting new issue ', err, data), res.json({error: 'required field(s) missing'})) : (console.log('new issue posted ', err, data), res.json(data));
+        err ? res.json({error: 'required field(s) missing'}) : res.json(data);
       });
     })
     
     .put(function (req, res){
       let project = req.params.project,
           Issue = mongoose.model(project, issue);
-      console.log('updating on ', project, 'the values ', req.body);
       if (!req.body._id) {
-        console.log('update missing id ', req.body);
         res.json({error: 'missing _id'});
       } else if ((Object.keys(req.body)).length < 2) {
-        console.log('update fields empty ', req.body);
         res.json({error: 'no update field(s) sent', '_id': req.body._id});
       } else {
         Issue.findOne({_id: req.body._id}, (err, doc) => {
           if (err || doc === null) {
-            console.log('error updating-finding ', err, doc);
             res.json({error: 'could not update', '_id': req.body._id});
           } else {
             for (let i of Object.keys(req.body)) {
               if(i !== '_id') doc[i] = req.body[i];
             }
             doc.updated_on = new Date();
-            doc.save((err, data) => {
-              err ? (console.log('error updating-saving ', err, data), res.json({error: 'could not update', '_id': req.body._id})) : (console.log('updated data ', data), res.json({result: 'successfully updated', '_id': req.body._id }));
+            doc.save((err) => {
+              err ? res.json({error: 'could not update', '_id': req.body._id}) : res.json({result: 'successfully updated', '_id': req.body._id });
             });
           }
         });
@@ -76,14 +70,12 @@ module.exports = function (app) {
     .delete(function (req, res){
       let project = req.params.project,
           Issue = mongoose.model(project, issue);
-      console.log('deleting on ', project, 'the value ', req.body);
       if (!req.body._id) {
-        console.log('error deleting missing id', req.body);
         res.json({error: 'missing _id'});
       } else {
         // Model.deleteOne doesn't pass the test here
         Issue.findOneAndDelete({_id: req.body._id}, (err, doc) => {
-          (err || doc === null) ? (console.log('error deleting-find ', err, doc), res.json({error: 'could not delete', '_id': req.body._id})) : (console.log('deleted id ', req.body), res.json({result: 'successfully deleted', '_id': req.body._id}));
+          (err || doc === null) ? res.json({error: 'could not delete', '_id': req.body._id}) : res.json({result: 'successfully deleted', '_id': req.body._id});
         });
       }
     });
